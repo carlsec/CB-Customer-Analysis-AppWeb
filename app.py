fadb31fe4d5e18a00bdd980b39dc5ead86494bea
Carlsec <carlsec@protonamil.com>
2021-01-11 16:38:01 -0300

    update

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model = joblib.load('clf_gb-model.pkl')
+model_gb = joblib.load('clf_gb-model.pkl')
+model_lgbm = joblib.load('clf_lgbm-model.pkl')
 d = joblib.load('d-model.pkl')
 
 @app.route('/')
@@ -20,8 +21,8 @@ def home():
 def predict():
 	customers = request.form['customers']
 	protests = np.array(request.form['protests'])
-	amountdue = np.array(request.form['amountdue'])
-	amountrent = np.array(request.form['amountrent'])
+	amountdue = np.array(request.form['amount due'])
+	amountrent = np.array(request.form['rent amount'])
 	city = np.array(request.form['city'])
 	commerce = np.array(request.form['commerce'])
 	consumes = np.array(request.form['consumes'])
@@ -32,18 +33,24 @@ def predict():
 	consumes = consumes.astype(int)
 	di = {'protests': protests, 'amountdue': amountdue, 'amountrent': amountrent, 'city': city, 'commerce': commerce, 'consumes': consumes, 'equipment': equipment}
 	df = pd.DataFrame(di,  index=[0])
-	df_cat = df[['city','commerce','equipment']]
+	df_cat = df[['city', 'commerce', 'equipment']]
 	df_cat = df_cat.apply(lambda x: d[x.name].fit_transform(x))
-	df_num = df[['amountrent', 'protests', 'amountdue', 'consumes']]
+	df_num = df[['protests', 'amountdue', 'amountrent','consumes']]
 	df = pd.concat([df_num,df_cat], axis = 1)
-	result = model.predict(df)
+	print(df.columns)
+	result_gb = model_gb.predict_proba(df)[:, 1]
+	result_lgbm = model_lgbm.predict_proba(df)[:, 1]
+	result = (result_gb*0.3) + (result_lgbm*0.7)
+	print(f"{result} AOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+	result = (result[0]).astype(float) * 100
+	result = round(result, 2)
 
-	if result == 1:
-		prediction = f"O cliente {customers} se enquadra nos clientes bons pagadores!"
+	if result > 50:
+		prediction = f"O cliente {customers} se enquadra nos clientes bons pagadores! com {result}% de chance de ser um bom pagador."
 	else:
-	    prediction = f"O cliente {customers} se enquadra nos clientes maus pagadores!"	
+	    prediction = f"O cliente {customers} se enquadra nos clientes maus pagadores! com {result}% de chance de ser um bom pagador."	
 
-	return render_template('template.html', prediction=f'{prediction}', Cliente = customers)
+	return render_template('template-pred.html', prediction=f'{prediction}')
 
 if __name__ == "__main__":
     app.run(debug=True)
